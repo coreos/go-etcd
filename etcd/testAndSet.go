e956d7085ef70a0cf3ed37f2639311774931286b
Xiang Li <xiang.li@coreos.com>
2013-07-14 16:29:16 -0700

    update testandset command

--- etcd/testAndSet.go
+++ etcd/testAndSet.go
@@ -2,21 +2,14 @@ package etcd
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"github.com/coreos/etcd/store"
 	"io/ioutil"
-	"net/http"
 	"net/url"
-	"path"
 )
 
 func TestAndSet(cluster string, key string, prevValue string, value string, ttl uint64) (*store.Response, bool, error) {
-
-	httpPath := path.Join(cluster, "/", version, "/testAndSet/", key)
-
-	//TODO: deal with https
-	httpPath = "http://" + httpPath
+	httpPath := getHttpPath("keys", key)
 
 	v := url.Values{}
 	v.Set("value", value)
@@ -26,34 +19,7 @@ func TestAndSet(cluster string, key string, prevValue string, value string, ttl 
 		v.Set("ttl", fmt.Sprintf("%v", ttl))
 	}
 
-	var resp *http.Response
-	var err error
-	// if we connect to a follower, we will retry until we found a leader
-	for {
-		client := http.Client{}
-		resp, err = client.PostForm(httpPath, v)
-
-		if resp != nil {
-
-			if resp.StatusCode == http.StatusTemporaryRedirect {
-				httpPath = resp.Header.Get("Location")
-
-				resp.Body.Close()
-
-				if httpPath == "" {
-					return nil, false, errors.New("Cannot get redirection location")
-				}
-
-				// try to connect the leader
-				continue
-			} else {
-				break
-			}
-
-		}
-
-		return nil, false, err
-	}
+	resp, err := sendRequest(httpPath, nil, &v)
 
 	b, err := ioutil.ReadAll(resp.Body)
 
