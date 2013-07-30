3033f164fe96d957190b5827bfd089a596158e96
Xiang Li <xiang.li@coreos.com>
2013-07-29 20:06:28 -0700

    add getfrom func

--- etcd/get.go
+++ etcd/get.go
@@ -23,10 +23,36 @@ func Get(key string) (*[]store.Response, error) {
 		return nil, err
 	}
 
+	return convertResponse(b)
+
+}
+
+// Get the value of the key from a given machine address
+// If the given machine is not available, return error
+// Mainly use for testing purpose
+func GetFrom(key string, addr string) (*[]store.Response, error) {
+	httpPath := createHttpPath(addr, path.Join(version, "keys", key))
+
+	resp, err := client.httpClient.Get(httpPath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	b, err := ioutil.ReadAll(resp.Body)
+
+	resp.Body.Close()
+
+	return convertResponse(b)
+}
+
+// convert byte stream to response
+func convertResponse(b []byte) (*[]store.Response, error) {
+
 	var results []store.Response
 	var result store.Response
 
-	err = json.Unmarshal(b, &result)
+	err := json.Unmarshal(b, &result)
 
 	if err != nil {
 		err = json.Unmarshal(b, &results)
@@ -39,7 +65,5 @@ func Get(key string) (*[]store.Response, error) {
 		results = make([]store.Response, 1)
 		results[0] = result
 	}
-
 	return &results, nil
-
 }
--- etcd/get_test.go
+++ etcd/get_test.go
@@ -19,4 +19,19 @@ func TestGet(t *testing.T) {
 		}
 		t.Fatalf("Get failed with %s %s %v", (*results)[0].Key, (*results)[0].Value, (*results)[0].TTL)
 	}
+
+	results, err = GetFrom("foo", "0.0.0.0:4001")
+
+	if err != nil || (*results)[0].Key != "/foo" || (*results)[0].Value != "bar" {
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Fatalf("Get failed with %s %s %v", (*results)[0].Key, (*results)[0].Value, (*results)[0].TTL)
+	}
+
+	results, err = GetFrom("foo", "0.0.0.0:4009")
+
+	if err == nil {
+		t.Fatal("should not get from port 4009")
+	}
 }
