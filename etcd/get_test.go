859ca971839bf743dabbfe8523289feeedb13e26
Derek Chiang <derekchiang93@gmail.com>
2013-10-09 04:14:17 -0400

    Some stylish changes

--- etcd/get_test.go
+++ etcd/get_test.go
@@ -16,10 +16,11 @@ func TestGet(t *testing.T) {
 
 	results, err := c.Get("foo")
 
-	if err != nil || results[0].Key != "/foo" || results[0].Value != "bar" {
-		if err != nil {
-			t.Fatal(err)
-		}
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if results[0].Key != "/foo" || results[0].Value != "bar" {
 		t.Fatalf("Get failed with %s %s %v", results[0].Key, results[0].Value, results[0].TTL)
 	}
 
@@ -31,10 +32,11 @@ func TestGet(t *testing.T) {
 
 	results, err = c.GetFrom("foo", "0.0.0.0:4001")
 
-	if err != nil || results[0].Key != "/foo" || results[0].Value != "bar" {
-		if err != nil {
-			t.Fatal(err)
-		}
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if results[0].Key != "/foo" || results[0].Value != "bar" {
 		t.Fatalf("Get failed with %s %s %v", results[0].Key, results[0].Value, results[0].TTL)
 	}
 
