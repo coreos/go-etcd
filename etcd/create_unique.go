c03fffc78df1fcce1c061cdab51425b3d7e35254
Derek Chiang <derekchiang93@gmail.com>
2013-10-18 18:43:22 -0400

    More comments

--- etcd/create_unique.go
+++ etcd/create_unique.go
@@ -1,9 +1,11 @@
 package etcd
 
+// Create a new directory with a random etcd-generated key under the given path.
 func (c *Client) CreateUniqueDir(key string, ttl uint64) (*Response, error) {
 	return c.post(key, "", ttl)
 }
 
+// Create a new file with a random etcd-generated key under the given path.
 func (c *Client) CreateUnique(key string, value string, ttl uint64) (*Response, error) {
 	return c.post(key, value, ttl)
 }
