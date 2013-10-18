693e65b8e003f460b3c143e47b7033af03903028
Derek Chiang <derekchiang93@gmail.com>
2013-10-18 05:29:26 -0400

    Adding more comments

--- etcd/delete.go
+++ etcd/delete.go
@@ -1,11 +1,13 @@
 package etcd
 
+// Delete the given directory and everything under it.
 func (c *Client) DeleteDir(key string) (*Response, error) {
 	return c.delete(key, options{
 		"recursive": true,
 	})
 }
 
+// Delete the given key.
 func (c *Client) Delete(key string) (*Response, error) {
 	return c.delete(key, nil)
 }
--- etcd/watch.go
+++ etcd/watch.go
@@ -16,29 +16,41 @@ var (
 // channel. And after someone receive the channel, it will go on to watch that prefix.
 // If a stop channel is given, client can close long-term watch using the stop channel
 
+// WatchDir watches any changes under the current directory, including all child
+// directories.  If a receiver channel is given, it will be a long-term watch. Watch will block at the
+// channel. And after someone receive the channel, it will go on to watch that prefix.
+// If a stop channel is given, client can close long-term watch using the stop channel
 func (c *Client) WatchDir(prefix string, receiver chan *Response, stop chan bool) (*Response, error) {
 	return c.watch(prefix, 0, true, receiver, stop)
 }
 
+// WatchDirIndex is the same as WatchDir, except that it scans from waitIndex onward
+// and returns the first change after waitIndex.
 func (c *Client) WatchDirIndex(prefix string, waitIndex uint64, receiver chan *Response, stop chan bool) (*Response, error) {
 	return c.watch(prefix, waitIndex, true, receiver, stop)
 }
 
+// Watch watches any changes to the given key, If a receiver channel is given, it will
+// be a long-term watch. Watch will block at the channel. And after someone receive the
+// channel, it will go on to watch that prefix.  If a stop channel is given, client can
+// close long-term watch using the stop channel
 func (c *Client) Watch(prefix string, receiver chan *Response, stop chan bool) (*Response, error) {
 	return c.watch(prefix, 0, false, receiver, stop)
 }
 
+// WatchIndex is the same as Watch, except that it scans from waitIndex onward
+// and returns the first change after waitIndex.
 func (c *Client) WatchIndex(prefix string, waitIndex uint64, receiver chan *Response, stop chan bool) (*Response, error) {
 	return c.watch(prefix, waitIndex, false, receiver, stop)
 }
 
-func (c *Client) watch(prefix string, waitIndex uint64, dir bool, receiver chan *Response, stop chan bool) (*Response, error) {
+func (c *Client) watch(prefix string, waitIndex uint64, recursive bool, receiver chan *Response, stop chan bool) (*Response, error) {
 	logger.Debugf("watch %s [%s]", prefix, c.cluster.Leader)
 	if receiver == nil {
-		return c.watchOnce(prefix, waitIndex, dir, stop)
+		return c.watchOnce(prefix, waitIndex, recursive, stop)
 	} else {
 		for {
-			resp, err := c.watchOnce(prefix, waitIndex, dir, stop)
+			resp, err := c.watchOnce(prefix, waitIndex, recursive, stop)
 			if resp != nil {
 				waitIndex = resp.Index + 1
 				receiver <- resp
@@ -53,7 +65,7 @@ func (c *Client) watch(prefix string, waitIndex uint64, dir bool, receiver chan 
 
 // helper func
 // return when there is change under the given prefix
-func (c *Client) watchOnce(key string, waitIndex uint64, dir bool, stop chan bool) (*Response, error) {
+func (c *Client) watchOnce(key string, waitIndex uint64, recursive bool, stop chan bool) (*Response, error) {
 
 	respChan := make(chan *Response)
 	errChan := make(chan error)
@@ -65,7 +77,7 @@ func (c *Client) watchOnce(key string, waitIndex uint64, dir bool, stop chan boo
 		if waitIndex > 0 {
 			options["waitIndex"] = waitIndex
 		}
-		if dir {
+		if recursive {
 			options["recursive"] = true
 		}
 
