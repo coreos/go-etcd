24ebd320a8b97fc839cc93d0bea6027e716fe619
Derek Chiang <derekchiang93@gmail.com>
2013-10-19 16:58:38 -0400

    Change API names and comments to clarify its functionality

--- etcd/delete.go
+++ etcd/delete.go
@@ -1,13 +1,17 @@
 package etcd
 
-// Delete the given directory and everything under it.
-func (c *Client) DeleteDir(key string) (*Response, error) {
+// DeleteAll deletes everything under the given key.  If the key
+// points to a file, the file will be deleted.  If the key points
+// to a directory, then everything under the directory, include
+// all child directories, will be deleted.
+func (c *Client) DeleteAll(key string) (*Response, error) {
 	return c.delete(key, options{
 		"recursive": true,
 	})
 }
 
-// Delete the given key.
+// Delete deletes the given key.  If the key points to a
+// directory, the method will fail.
 func (c *Client) Delete(key string) (*Response, error) {
 	return c.delete(key, nil)
 }
