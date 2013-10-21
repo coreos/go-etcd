ba6ab860e091dca41e8b915e6996022c51171602
Derek Chiang <derekchiang93@gmail.com>
2013-10-21 04:58:34 -0400

    Change the watch API

--- etcd/watch.go
+++ etcd/watch.go
@@ -9,39 +9,28 @@ var (
 	ErrWatchStoppedByUser = errors.New("Watch stopped by the user via stop channel")
 )
 
-// Watch any change under the given prefix.
-// When a sinceIndex is given, watch will try to scan from that index to the last index
-// and will return any changes under the given prefix during the history
+// WatchAll returns the first change under the given prefix since the given index.  To
+// watch for the latest change, set waitIndex = 0.
+//
+// If the prefix points to a directory, any change under it, including all child directories,
+// will be returned.
+//
 // If a receiver channel is given, it will be a long-term watch. Watch will block at the
 // channel. And after someone receive the channel, it will go on to watch that prefix.
 // If a stop channel is given, client can close long-term watch using the stop channel
-
-// WatchDir watches any changes under the current directory, including all child
-// directories.  If a receiver channel is given, it will be a long-term watch. Watch will block at the
-// channel. And after someone receive the channel, it will go on to watch that prefix.
-// If a stop channel is given, client can close long-term watch using the stop channel
-func (c *Client) WatchDir(prefix string, receiver chan *Response, stop chan bool) (*Response, error) {
-	return c.watch(prefix, 0, true, receiver, stop)
-}
-
-// WatchDirIndex is the same as WatchDir, except that it scans from waitIndex onward
-// and returns the first change after waitIndex.
-func (c *Client) WatchDirIndex(prefix string, waitIndex uint64, receiver chan *Response, stop chan bool) (*Response, error) {
+func (c *Client) WatchAll(prefix string, waitIndex uint64, receiver chan *Response, stop chan bool) (*Response, error) {
 	return c.watch(prefix, waitIndex, true, receiver, stop)
 }
 
-// Watch watches any changes to the given key, If a receiver channel is given, it will
-// be a long-term watch. Watch will block at the channel. And after someone receive the
-// channel, it will go on to watch that prefix.  If a stop channel is given, client can
-// close long-term watch using the stop channel
-func (c *Client) Watch(prefix string, receiver chan *Response, stop chan bool) (*Response, error) {
-	return c.watch(prefix, 0, false, receiver, stop)
-}
-
-// WatchIndex is the same as Watch, except that it scans from waitIndex onward
-// and returns the first change after waitIndex.
-func (c *Client) WatchIndex(prefix string, waitIndex uint64, receiver chan *Response, stop chan bool) (*Response, error) {
-	return c.watch(prefix, waitIndex, false, receiver, stop)
+// Watch returns the first change to the given key since the given index.  To
+// watch for the latest change, set waitIndex = 0.
+//
+// If a receiver channel is given, it will be a long-term watch. Watch will block at the
+// channel. And after someone receive the channel, it will go on to watch that
+// prefix.  If a stop channel is given, client can close long-term watch using
+// the stop channel
+func (c *Client) Watch(key string, waitIndex uint64, receiver chan *Response, stop chan bool) (*Response, error) {
+	return c.watch(key, waitIndex, false, receiver, stop)
 }
 
 func (c *Client) watch(prefix string, waitIndex uint64, recursive bool, receiver chan *Response, stop chan bool) (*Response, error) {
