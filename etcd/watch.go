ddc04357a71157c062813a5bab0737948f808535
Wei <jrweizhang@gmail.com>
2013-11-15 14:02:54 +0800

    Fix weird watching behavior.

--- etcd/watch.go
+++ etcd/watch.go
@@ -41,7 +41,7 @@ func (c *Client) watch(prefix string, waitIndex uint64, recursive bool, receiver
 		for {
 			resp, err := c.watchOnce(prefix, waitIndex, recursive, stop)
 			if resp != nil {
-				waitIndex = resp.ModifiedIndex + 1
+				waitIndex = resp.ModifiedIndex
 				receiver <- resp
 			} else {
 				return nil, err
