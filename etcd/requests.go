e1cf64dec74adf90a295ec8034a41ea6e1cebdc6
Xiang Li <xiang.li@coreos.com>
2013-11-24 15:34:04 -0500

    refactor typo

--- etcd/requests.go
+++ etcd/requests.go
@@ -197,7 +197,7 @@ func (c *Client) sendRequest(method string, relativePath string,
 // handle HTTP response
 // If status code is OK, read the http body.
 // If status code is TemporaryRedirect, update leader.
-// If status coid is InternalServerError, sleep for 200ms.
+// If status code is InternalServerError, sleep for 200ms.
 func (c *Client) handleResp(resp *http.Response) (bool, []byte) {
 	defer resp.Body.Close()
 
