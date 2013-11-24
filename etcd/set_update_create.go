fd2e43a89e79ffc9afef5a696613328effcab8ab
Xiang Li <xiang.li@coreos.com>
2013-11-24 16:02:55 -0500

    feat add rawresponse to set func family

--- etcd/set_update_create.go
+++ etcd/set_update_create.go
@@ -2,8 +2,7 @@ package etcd
 
 // SetDir sets the given key to a directory.
 func (c *Client) SetDir(key string, ttl uint64) (*Response, error) {
-
-	raw, err := c.put(key, "", ttl, nil)
+	raw, err := c.RawSetDir(key, ttl)
 
 	if err != nil {
 		return nil, err
@@ -15,11 +14,7 @@ func (c *Client) SetDir(key string, ttl uint64) (*Response, error) {
 // UpdateDir updates the given key to a directory.  It succeeds only if the
 // given key already exists.
 func (c *Client) UpdateDir(key string, ttl uint64) (*Response, error) {
-	ops := options{
-		"prevExist": true,
-	}
-
-	raw, err := c.put(key, "", ttl, ops)
+	raw, err := c.RawUpdateDir(key, ttl)
 
 	if err != nil {
 		return nil, err
@@ -31,11 +26,7 @@ func (c *Client) UpdateDir(key string, ttl uint64) (*Response, error) {
 // UpdateDir creates a directory under the given key.  It succeeds only if
 // the given key does not yet exist.
 func (c *Client) CreateDir(key string, ttl uint64) (*Response, error) {
-	ops := options{
-		"prevExist": false,
-	}
-
-	raw, err := c.put(key, "", ttl, ops)
+	raw, err := c.RawCreateDir(key, ttl)
 
 	if err != nil {
 		return nil, err
@@ -46,7 +37,7 @@ func (c *Client) CreateDir(key string, ttl uint64) (*Response, error) {
 
 // Set sets the given key to the given value.
 func (c *Client) Set(key string, value string, ttl uint64) (*Response, error) {
-	raw, err := c.put(key, value, ttl, nil)
+	raw, err := c.RawSet(key, value, ttl)
 
 	if err != nil {
 		return nil, err
@@ -58,11 +49,7 @@ func (c *Client) Set(key string, value string, ttl uint64) (*Response, error) {
 // Update updates the given key to the given value.  It succeeds only if the
 // given key already exists.
 func (c *Client) Update(key string, value string, ttl uint64) (*Response, error) {
-	ops := options{
-		"prevExist": true,
-	}
-
-	raw, err := c.put(key, value, ttl, ops)
+	raw, err := c.RawUpdate(key, value, ttl)
 
 	if err != nil {
 		return nil, err
@@ -74,15 +61,51 @@ func (c *Client) Update(key string, value string, ttl uint64) (*Response, error)
 // Create creates a file with the given value under the given key.  It succeeds
 // only if the given key does not yet exist.
 func (c *Client) Create(key string, value string, ttl uint64) (*Response, error) {
-	ops := options{
-		"prevExist": false,
-	}
-
-	raw, err := c.put(key, value, ttl, ops)
+	raw, err := c.RawCreate(key, value, ttl)
 
 	if err != nil {
 		return nil, err
 	}
 
 	return raw.toResponse()
+}
+
+func (c *Client) RawSetDir(key string, ttl uint64) (*RawResponse, error) {
+	return c.put(key, "", ttl, nil)
+}
+
+func (c *Client) RawUpdateDir(key string, ttl uint64) (*RawResponse, error) {
+	ops := options{
+		"prevExist": true,
+	}
+
+	return c.put(key, "", ttl, ops)
+}
+
+func (c *Client) RawCreateDir(key string, ttl uint64) (*RawResponse, error) {
+	ops := options{
+		"prevExist": false,
+	}
+
+	return c.put(key, "", ttl, ops)
+}
+
+func (c *Client) RawSet(key string, value string, ttl uint64) (*RawResponse, error) {
+	return c.put(key, value, ttl, nil)
+}
+
+func (c *Client) RawUpdate(key string, value string, ttl uint64) (*RawResponse, error) {
+	ops := options{
+		"prevExist": true,
+	}
+
+	return c.put(key, value, ttl, ops)
+}
+
+func (c *Client) RawCreate(key string, value string, ttl uint64) (*RawResponse, error) {
+	ops := options{
+		"prevExist": false,
+	}
+
+	return c.put(key, value, ttl, ops)
 }
